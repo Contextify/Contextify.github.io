43162e90cc8c0707443a72997d4c903579a63cd5
chaitr91 <chaitrar91@gmail.com>
2016-12-08 02:46:01 -0500

    StartandEndEpoch

--- js/timelineImpl.js
+++ js/timelineImpl.js
@@ -69,7 +69,91 @@ move:function(moveVal) {
 moveToCurrentTime:function() {
 
             timelineImpl.timeline.setVisibleChartRangeNow();
+        },
+
+    setTimeRangeScale:function(){
+            var newStartDate = new Date(document.getElementById('startDate').value);
+            var newEndDate   = new Date(document.getElementById('endDate').value);
+            var strDate=timelineImpl.dateFormat(newStartDate);
+			var endDate=timelineImpl.dateFormat(newEndDate);
+			console.log("strDate"+strDate);
+            console.log("strDate"+endDate);
+            
+            var startEpoch = moment(strDate).unix();
+            var endEpoch = moment(endDate).unix()
+         timelineImpl.loadDataForRangeChange(startEpoch,endEpoch);
+
+    },
+
+
+loadDataForRangeChange:function(startEpoch,endEpoch){
+        $.ajax({
+        	  url: "https://home.sriramsv.com:5000/user/Sriram?start="+startEpoch+"&end="+endEpoch,
+        	  //data: data,
+			  dataType:'json',
+            
+        	  success: function(data) {
+                 
+                 console.log(JSON.stringify(data));
+				 console.log("Lt it be");
+    // specify options
+        var options = {
+          "width":  "100%",
+          "height": "75%",
+           // optional
+        };
+
+        // Instantiate our timeline object.
+        timelineImpl.timeline = new links.Timeline(document.getElementById('mytimeline'));
+        timelineImpl.timeline.setOptions(options)
+        timelineImpl.timeline.setData(data)
+        // Draw our timeline with the created data and options
+        timelineImpl.timeline.draw(data);
+              }
+              
+        	  //dataType: "json"
+        	});
+},
+
+   // adjust start and end time.
+        setTime:function() {
+            if (!timeline) return;
+
+            var newStartDate = new Date(document.getElementById('startDate').value);
+            var newEndDate   = new Date(document.getElementById('endDate').value);
+            timeline.setVisibleChartRange(newStartDate, newEndDate);
+            timeline.redraw();
+        },
+
+        onrangechange:function() {
+            // adjust the values of startDate and endDate
+            var range = timeline.getVisibleChartRange();
+            document.getElementById('startDate').value = dateFormat(range.start);
+            document.getElementById('endDate').value   = dateFormat(range.end);
+        },
+
+        onselect:function() {
+            var sel = timeline.getSelection();
+            if (sel.length) {
+                if (sel[0].row != undefined) {
+                    var row = sel[0].row;
+                    alert("event " + row + " selected");
+                }
+            }
+        },
+
+        // Format given date as "yyyy-mm-dd hh:ii:ss"
+        // @param datetime   A Date object.
+        dateFormat:function(date) {
+            var datetime = date.getFullYear() + "-" +
+                    ((date.getMonth()   <  9) ? "0" : "") + (date.getMonth() + 1) + "-" +
+                    ((date.getDate()    < 10) ? "0" : "") +  date.getDate() + " " +
+                    ((date.getHours()   < 10) ? "0" : "") +  date.getHours() + ":" +
+                    ((date.getMinutes() < 10) ? "0" : "") +  date.getMinutes() + ":" +
+                    ((date.getSeconds() < 10) ? "0" : "") +  date.getSeconds();
+            return datetime;
         }
+
 
     };
 
